dcef5f0252e0365aec1c52ffd74ab21c8fe6cc48
AKSourav <97042110+AKSourav@users.noreply.github.com>
2022-01-03 21:40:07 +0530

    Update contributors.js

--- data/contributors.js
+++ data/contributors.js
@@ -44,6 +44,15 @@ const contributors = [
         github : "https://github.com/Kingshuk3006",
         gender : "Male"
     },
+    {
+        name : "Anupam Kumar Sourav",
+        branch : "CSE DS",
+        roll : 2162017,
+        year : 2025,
+        linkedin : "",
+        github : "https://github.com/AKSourav",
+        gender : "male"
+    },
 ]
 
 export default contributors
